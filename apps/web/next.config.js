ceaee0b3eece557a81fa019327bab8339cece5ec
ThanhTung19431261 <90313619+ThanhTung19431261@users.noreply.github.com>
2025-07-13 14:30:23 +0700

    Finance manager

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+// const withTM = require('next-transpile-modules')(['@babel/preset-react']);
+//   '@fullcalendar/common',
+//   '@fullcalendar/common',
+//   '@fullcalendar/daygrid',
+//   '@fullcalendar/interaction',
+//   '@fullcalendar/react',
+
+const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'i.ibb.co',
+      'scontent.fotp8-1.fna.fbcdn.net',
+    ],
+    // Make ENV
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
